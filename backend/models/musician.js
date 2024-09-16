2b907460dc7563ae832a4da050de196d906f8e98
jaeahn2010 <jaeahn2010@gmail.com>
2024-09-15 18:26:58 -0700

    removed unnecessary fields from musiciansSchema

--- backend/models/musician.js
+++ backend/models/musician.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 
 const musicianSchema = new mongoose.Schema(
     {
-        userCategory: {type: String, required: true, default: 'musician'},
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
         instruments: [{ type: String }],
         email: { type: String, required: true, unique: true },
-        password: { type: String, required: true, minLength: 8 },
-        description: { type: String },
         availableRepertoire: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Opus'}],
-        isAdmin: { type: Boolean, required: true, default: false}
     },
 );
 
