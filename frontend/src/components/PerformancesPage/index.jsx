c584bf339d45db3c5117900d29bfd0e8476a6355
jaeahn2010 <jaeahn2010@gmail.com>
2024-11-18 00:24:22 -0800

    made small adjustments to performance page to account for time zones; added two actual performance objects

--- frontend/src/components/PerformancesPage/index.jsx
+++ frontend/src/components/PerformancesPage/index.jsx
@@ -58,13 +58,13 @@ export default function PerformancesPage({ isMenuOpen }) {
             ? <section className="career-items my-24 min-h-[25rem]">
                 <h1 className="text-xl text-center my-10 lg:text-3xl">Our performances</h1>
                 {performances.map((performance, index) => 
-                    <div key={index} className='card-flip flex my-5 w-11/12 md:w-3/4 h-[5rem] md:h-[10rem] mx-auto border-2 border-stone-200 bg-gradient-to-r from-stone-800 via-stone-500 to-stone-800'>
+                    <div key={index} className='card-flip flex my-5 w-11/12 md:w-3/4 h-[5rem] md:h-[10rem] mx-auto border border-stone-200 rounded-xl bg-gradient-to-r from-stone-800 via-stone-500 to-stone-800'>
                         <div className="card-front flex w-full">
                             <div className="w-full flex flex-col items-center justify-center">
                                 <p className="text-sm md:text-2xl my-3">{performance.title}</p>
                                 <div className='flex justify-between items-center w-5/6'>
                                     <p>{performance.locationName} ({performance.city}, {performance.state})</p>
-                                    <p className={performance.date > new Date() ? 'text-green-300' : 'text-red-403'}>{new Date(performance.date).toLocaleDateString()}, {nonMilitaryTime(performance.time)}</p>
+                                    <p className={performance.date > new Date() ? 'text-green-300' : 'text-red-403'}>{new Date(new Date(performance.date).getTime() + 8 * 60 * 60 * 1000).toLocaleDateString()}, {nonMilitaryTime(performance.time)}</p>
                                 </div>
                                 <p className='my-3'>Featured GLVMS Musician(s): {performance.featuredGLVMSMusicians.map((musicianId, index) => {
                                     let musician = musicians.find(musician => musician._id === musicianId)
@@ -73,13 +73,14 @@ export default function PerformancesPage({ isMenuOpen }) {
                             </div>
                         </div>
                         <div className="hidden card-back flex flex-col items-center justify-center w-full h-full">
-                            <div>  
+                            <div className='w-11/12 mx-auto'>  
                                 {performance.collaborators.length
                                 ? <>
                                     <p className='text-center underline'>Also featuring:</p>
                                     {performance.collaborators.map(collaborator =>
-                                        <p key={collaborator.collaboratorLastName + collaborator.collaboratorFirstName}>{collaborator.collaboratorFirstName} {collaborator.collaboratorLastName} ({collaborator.collaboratorInstrument})</p>
+                                        <p key={collaborator.collaboratorLastName + collaborator.collaboratorFirstName} className='text-center'>{collaborator.collaboratorFirstName} {collaborator.collaboratorLastName} ({collaborator.collaboratorInstrument})</p>
                                     )}
+                                    <p className='my-3'>{performance.description}</p>
                                 </>
                                 : ''}
                             </div>	
