d4af7566e782dc51d825c0638c6d93d5f23cfb75
jaeahn2010 <jaeahn2010@gmail.com>
2024-11-25 23:44:54 -0800

    fixed repertoire filter, but may need to fix it further

--- frontend/src/components/RepertoirePage/index.jsx
+++ frontend/src/components/RepertoirePage/index.jsx
@@ -2,77 +2,117 @@ import { useState, useEffect } from 'react'
 import Gallery from '../Gallery'
 
 export default function RepertoirePage({ isMenuOpen, allOpuses, allComposers, setOpusDetails, loginStatus, instruments, scrollToTop, sortObjects }) {
+    const [showFilters, setShowFilters] = useState(false)
+    const [filterSwitch, setFilterSwitch] = useState({
+        composer: [false, ''],
+        instrumentation: [false, ''],
+        price: [false, 0],
+        keyword: [false, ''],
+    })
     const [filteredOpuses, setFilteredOpuses] = useState([])
     const filterDivStyle = 'flex justify-center items-center w-11/12 mx-auto my-2'
     const filterLabelStyle = 'w-1/2 text-right mr-2 text-sm'
     const filterInputStyle = 'w-1/2 text-stone-800 text-sm'
-    
-    // get filtered opus data
-	function getFilteredOpusData(category, filter) {
-        let filteredOpusData = []
-        switch(category) {
-            case 'composer':
-                filteredOpusData = allOpuses.filter(opus => opus.composer === filter)
-                break
-            case 'instrumentation':
-                filteredOpusData = allOpuses.filter(opus => opus.instrumentation.includes(filter))
-                break
-            case 'price':
-                filteredOpusData = allOpuses.filter(opus => opus.price <= filter && opus.price !== null)
-                break
-            case 'keyword':
-                filteredOpusData = allOpuses.filter(opus => opus.title.includes(filter))
-                break
+    const btnStyle = 'mx-auto my-5 p-2 border border-stone-200 rounded-xl hover:bg-amber-400 hover:text-stone-800 bg:scale-105'
+    const arrowStyle = `text-xl duration-500 ${showFilters ? 'rotate-180' : ''}`
+
+    function handleChange(evt) {
+        setFilterSwitch({
+            ...filterSwitch,
+            [evt.target.id]: [true, evt.target.value]
+        })
+    }
+
+    function applyFilters(evt) {
+        for (let category in filterSwitch) {
+            let [switchOn, switchValue] = filterSwitch[category]
+            if (switchOn) {
+                switch(category) {
+                    case 'composer':
+                        setFilteredOpuses(filteredOpus => filteredOpus.filter(opus => opus.composer === switchValue))
+                        break
+                    case 'instrumentation':
+                        setFilteredOpuses(filteredOpus => filteredOpus.filter(opus => opus.instrumentation.includes(switchValue)))
+                        break
+                    case 'price':
+                        setFilteredOpuses(filteredOpus => filteredOpus.filter(opus => opus.price <= switchValue && opus.price !== null))
+                        break
+                    case 'keyword':
+                        setFilteredOpuses(filteredOpus => filteredOpus.filter(opus => opus.title.includes(switchValue)))
+                        break
+                }
+            }
         }
-        setFilteredOpuses(sortObjects(filteredOpusData))
-	}
+    }
 
     useEffect(() => {
         setFilteredOpuses(allOpuses)
         scrollToTop()
     }, [])
 
+    useEffect(() => {
+        setFilteredOpuses(allOpuses)
+    }, [filterSwitch])
+
     return (
         <main className={`${isMenuOpen ? 'z-0 opacity-5' : ''} text-stone-200 font-poppins min-h-[125vh]`}>
             <h1 className="text-xl text-center my-10 lg:text-3xl">Available Repertoire</h1>
-            <section className="border border-stone-400 rounded-xl my-2 py-2 w-11/12 lg:w-1/2 mx-auto">
-                <p className="text-center underline">FILTERS</p>
+            <div className='w-1/6 mx-auto flex justify-around items-center'>
+                <p className={arrowStyle}>&#10507;</p>
+                <button
+                    onClick={() => setShowFilters(!showFilters)}
+                    className={`${showFilters ? 'text-red-300' : 'text-green-300'} text-center`}
+                >{showFilters ? 'HIDE FILTERS' : 'SHOW FILTERS'}</button>
+                <p className={arrowStyle}>&#10507;</p>
+            </div>
+            <section className={`${showFilters ? '' : 'hidden'} duration-500 border border-stone-400 rounded-xl my-2 py-2 w-11/12 lg:w-1/2 mx-auto`}>
                 <div className="w-full flex flex-col">
                     <div className={filterDivStyle}>
-                        <label className={filterLabelStyle} htmlFor='composerFilter'>COMPOSER</label>
+                        <label className={filterLabelStyle} htmlFor='composer'>COMPOSER</label>
                         <select
                             className={filterInputStyle}
-                            name="composerFilter"
-                            id="composerFilter"
+                            name="composer"
+                            id="composer"
                             defaultValue={0}
-                            onChange={evt => getFilteredOpusData('composer', evt.target.value)}>
+                            onChange={handleChange}>
                             <option key={0} value={0} disabled>Select a composer</option>
                             {allComposers.sort().map(composer => <option key={composer} value={composer}>{composer}</option>)}
                         </select>
                     </div>
                     <div className={filterDivStyle}>
-                        <label className={filterLabelStyle} htmlFor='instrumentationFilter'>INSTRUMENTATION</label>
+                        <label className={filterLabelStyle} htmlFor='instrumentation'>INSTRUMENTATION</label>
                         <select
                             className={filterInputStyle}
-                            name="instrumentationFilter"
-                            id="instrumentationFilter"
+                            name="instrumentation"
+                            id="instrumentation"
                             defaultValue={0}
-                            onChange={evt => getFilteredOpusData('instrumentation', evt.target.value)}>
+                            onChange={handleChange}>
                             <option key={0} value={0} disabled>Select an instrument</option>
                             {instruments.map(instrument => <option key={instrument} value={instrument}>{instrument}</option>)}
                         </select>
                     </div>
                     <div className={filterDivStyle}>
-                        <label className={filterLabelStyle} htmlFor='priceFilter'>MAXIMUM PRICE</label>
+                        <label className={filterLabelStyle} htmlFor='price'>MAXIMUM PRICE</label>
                         <input
                             className={filterInputStyle + ' px-1'}
-                            name="priceFilter"
-                            id="priceFilter"
+                            name="price"
+                            id="price"
                             defaultValue={0}
-                            onChange={evt => getFilteredOpusData('price', evt.target.value)}
+                            onChange={handleChange}
                         />
                     </div>
-                    <button className='mx-auto my-5 p-2 border border-stone-200 rounded-xl hover:bg-amber-400 hover:text-stone-800 bg:scale-105' onClick={() => setFilteredOpuses(allOpuses)}>CLEAR</button>
+                    <div className='flex justify-around items-center'>
+                        <button className={btnStyle} onClick={applyFilters}>APPLY</button>
+                        <button className={btnStyle} onClick={() => {
+                            setFilteredOpuses(allOpuses)
+                            setFilterSwitch({
+                                composer: [false, ''],
+                                instrumentation: [false, ''],
+                                price: [false, 0],
+                                keyword: [false, ''],
+                            })
+                        }}>CLEAR</button>
+                    </div>
                 </div>
             </section>
             <Gallery
